39a81c5636dc79b35f86a413836dbf28bb2811c8
Andrey <raigon28@gmail.com>
2021-11-05 03:42:12 +0300

    react configuration

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    index: ["@babel/polyfill", "./index.jsx"],
+  },
+  output: {
+    filename: "[name].[hash].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json', '.scss']
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|gig)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"]
+          }
+        }
+      }
+    ],
+  },
+};
