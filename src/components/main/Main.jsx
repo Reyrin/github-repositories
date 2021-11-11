178bd7eeb1d82aefc1742c5c882195daa384eb44
Andrey <raigon28@gmail.com>
2021-11-11 03:01:05 +0300

    pagination

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,20 +3,29 @@ import "./main.scss";
 import { getRepos } from "./../actions/repos";
 import { useDispatch, useSelector } from "react-redux";
 import Repo from "./repo/Repo";
+import { setCurrentPage } from "../../reducers/reposReducer";
+import { createPages } from "../../utils/pagesCreator";
 
 const Main = () => {
   const dispatch = useDispatch();
   const repos = useSelector((state) => state.repos.items);
   const isFetching = useSelector((state) => state.repos.isFetching);
+  const currentPage = useSelector((state) => state.repos.currentPage);
+  const totalCount = useSelector((state) => state.repos.totalCount);
+  const perPage = useSelector((state) => state.repos.perPage);
+  const pagesCount = Math.ceil(totalCount / perPage);
+  const pages = [];
+  createPages(pages, pagesCount, currentPage);
 
-  const [text, setText] = React.useState('');
+  const [text, setText] = React.useState("");
 
   React.useEffect(() => {
-    dispatch(getRepos());
-  }, []);
+    dispatch(getRepos(text, currentPage, perPage));
+  }, [currentPage]);
 
   function searchHandler() {
-    dispatch(getRepos(text));
+    dispatch(setCurrentPage(1));
+    dispatch(getRepos(text, currentPage, perPage));
   }
 
   return (
@@ -29,13 +38,36 @@ const Main = () => {
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button className="search-btn" onClick={() => searchHandler()}>Search</button>
+        <button className="search-btn" onClick={() => searchHandler()}>
+          Search
+        </button>
       </div>
       {isFetching ? (
-        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       ) : (
         repos.map((repo, index) => <Repo repo={repo} key={index} />)
       )}
+
+      <div className="pages">
+        {pages.map((page, index) => (
+          <span
+            key={index}
+            className={currentPage == page ? "current-page" : "page"}
+            onClick={() => dispatch(setCurrentPage(page))}
+          >
+            {page}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
